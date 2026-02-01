41242e429591d70c1f0c6dbe61e66ce89090be1c
Kanishka Mohan <contact@kanishkamohan.com>
2026-02-01 18:14:55 +0530

    Add blog & redirect, Add 404, Update icons, Configure sitemap and analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,27 @@
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
-import cloudflare from '@astrojs/cloudflare';
+//import cloudflare from '@astrojs/cloudflare';
 import { defineConfig } from 'astro/config';
+
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
 	// Required for sitemap -> Replace with your site's URL
-	site: 'https://astro-simple-portfolio.pages.dev/',
+	site: 'https://kanishkamohan.com',
+
 	output: 'static',
 	integrations: [sitemap()],
-	adapter: cloudflare({
-		imageService: 'cloudflare',
-		platformProxy:{
-			enabled: true
-		}
-	}),
+
+	//adapter: cloudflare({
+	//	imageService: 'cloudflare',
+	//	platformProxy: {
+	//		enabled: true,
+	//	},
+	//}),
 	vite: {
 		plugins: [tailwindcss()],
 	},
+
+	adapter: vercel(),
 });
